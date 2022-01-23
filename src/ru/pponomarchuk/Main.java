3eb524cd9c96621abc848a26a813bdaa1b87322c
Pavel <ponomarchuk@yandex.ru>
2022-01-23 23:59:32 +0500

    Initial commit

---
+++ src/ru/pponomarchuk/Main.java
@@ -0,0 +1,56 @@
+package ru.pponomarchuk;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Node root = new Node();
+        root.setValue(1);
+
+        Node node1 = new Node();
+        node1.setValue(2);
+        Node node2 = new Node();
+        node2.setValue(3);
+        root.setLeftChild(node1);
+        root.setRightChild(node2);
+
+        Node node3 = new Node();
+        node3.setValue(4);
+        node1.setLeftChild(node3);
+
+        System.out.println(getSumByLayers(root));
+    }
+
+    private static int getSumByLayers(Node root) {
+        int sum = 0;
+        boolean nextLayerExist = root.getLeftChild() != null || root.getRightChild() != null;
+        ArrayList<Node> nodes = new ArrayList<>();
+        nodes.add(root);
+
+        if (!nextLayerExist) {
+            sum = root.getValue();
+        }
+
+        while (nextLayerExist) {
+            nextLayerExist = false;
+            ArrayList<Node> currentLayerNodes = new ArrayList<>();
+
+            for (Node item: nodes) {
+                sum += item.getValue();
+
+                if (item.getLeftChild() != null){
+                    currentLayerNodes.add(item.getLeftChild());
+                }
+                if (item.getRightChild() != null) {
+                    currentLayerNodes.add(item.getRightChild());
+                }
+                if (item.getRightChild() != null || item.getLeftChild() != null) {
+                    nextLayerExist = true;
+                }
+            }
+            nodes = currentLayerNodes;
+        }
+        return sum;
+    }
+}
---
+++ src/ru/pponomarchuk/Node.java
@@ -0,0 +1,31 @@
+package ru.pponomarchuk;
+
+public class Node {
+    private Node leftChild;
+    private Node rightChild;
+    private int value;
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
